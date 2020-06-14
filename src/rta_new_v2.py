f23c9c51affb01c3fa1f0584af4c38f5f41adb79
automaticdai <automatic.dai@gmail.com>
2020-06-15 00:17:36 +0100

    improved rta_new

--- src/rta_new_v2.py
+++ src/rta_new_v2.py
@@ -7,6 +7,7 @@ import pickle
 import networkx as nx
 
 from graph import find_longest_path_dfs, find_predecesor, find_successor, find_ancestors, find_descendants, get_subpath_between
+from rta_np import rta_np
 
 
 def EO():
@@ -19,6 +20,8 @@ def EO():
 def load_task(task_idx):
     # << load DAG task <<
     dag_task_file = "../dag-gen-rnd/data/Tau_{:d}.gpickle".format(task_idx)
+
+    # task is saved as NetworkX gpickle format
     G = nx.read_gpickle(dag_task_file)
 
     # formulate the graph list
@@ -64,6 +67,12 @@ def load_task(task_idx):
     return G_dict, C_dict, lamda, VN_array, L, W
 
 
+def rta_classic(task_idx, m):
+    G_dict, C_dict, lamda, VN_array, L, W = load_task(task_idx)
+    makespan = L + 1 / m * (W - L)
+    return makespan
+
+
 def remove_nodes_in_list(nodes, nodes_to_remove):
     for i in nodes.copy():
         if i in nodes_to_remove:
@@ -71,6 +80,8 @@ def remove_nodes_in_list(nodes, nodes_to_remove):
 
 
 def find_concurrent_nodes(G, node):
+    ''' find concurrent nodes
+    '''
     ancs = find_ancestors(G, node, path=[])
     decs = find_descendants(G, node, path=[])
     
@@ -82,14 +93,21 @@ def find_concurrent_nodes(G, node):
     return V
 
 
-def test_parallelism(G, n):
+def find_interference_nodes(G, node, candidate_list):
+    ''' find interfernce nodes from a candidate list
+    '''
     pass
 
 
+def test_parallelism(G, node, n):
+    ''' test if delta(node) < n
+    '''
+    return True
+
+
 def rta_new_v2(task_idx, m):
-    R = 0
-    alpha = 0
-    beta = 0
+    alpha_sum = 0
+    beta_sum = 0
 
     providers = []
     consumers = []
@@ -143,7 +161,8 @@ def rta_new_v2(task_idx, m):
                 consumers.append(new_consumer)
 
                 # remove from NC list
-                for i_nc in new_consumer:  nc_nodes_left.remove(i_nc)
+                for i_nc in new_consumer:
+                    nc_nodes_left.remove(i_nc)
 
                 new_consumer = []
         else:
@@ -155,31 +174,123 @@ def rta_new_v2(task_idx, m):
             nc_nodes_left.sort()
             consumers.append(nc_nodes_left)
 
-    print(providers)
-    print(consumers)
+    print("Providers:", providers)
+    print("Consumers:", consumers)
 
     # --------------------------------------------------------------------------
-    # III. bound alpha and beta
-    interference = 0
-    print(find_concurrent_nodes(G_dict, 4))
+    # III. calculate the finish times of each provider, and the consumers within
+    f_dict = {}
+    f_offest = 0
+    #R_m_minus_one = []
+    #R_minus_one = []
 
-    
+    # iteratives all providers
+    for i, theta_i_star in enumerate(providers):
+        print("theta", i, ":", theta_i_star)
+        print(consumers[i])  
+
+        # get the finish time of all provider nodes
+        for provi_idx, provi_i in enumerate(theta_i_star):
+            # (skipped) topoligical order, skipped because guaranteed by the generator
+            if provi_idx == 0:
+                f_i = C_dict[provi_i] + f_offest
+                f_dict[provi_i] = f_i
+                f_theta_i_star = f_i
+            else:
+                previous_i = theta_i_star[provi_idx - 1]
+                f_i = C_dict[provi_i] + f_dict[previous_i] + f_offest
+                f_dict[provi_i] = f_i
+                f_theta_i_star = f_i
+        
+        # iteratives all consumers
+        # (skipped) topoligical order, skipped because guaranteed by the generator
+        # note: consumer can be empty
+        theta_i = consumers[i]
+        f_v_i_max = 0; f_v_i_max_idx = -1
+        for _, theta_ij in enumerate(theta_i):
+            print(theta_ij, ":", C_dict[theta_ij])
+            
+            # the interference term
+            if test_parallelism(G_dict, theta_ij, m-1):
+                interference = 0
+            else:
+                interference = 0
+
+            # find the max finish time of all its predecences
+            f_ij_pre_max = 0
+
+            predecsor_of_ij = find_predecesor(G_dict, theta_ij)
+            for pre_i in predecsor_of_ij:
+                if f_dict[pre_i] > f_ij_pre_max:
+                    f_ij_pre_max = f_dict[pre_i]
+
+            # concurrent nodes of ij. Can be empty.
+            con_ij = find_concurrent_nodes(G_dict, theta_ij)  
+
+            # calculate the finish time
+            f_ij = C_dict[theta_ij] + interference + f_ij_pre_max
+            f_dict[theta_ij] = f_ij
+
+            if f_ij > f_v_i_max:
+                f_v_i_max = f_ij
+                f_v_i_max_idx = theta_ij
+        
+        R_i_m_minus_one = f_v_i_max
+
+        # start to calculate the response time of provider i
+        Wi_nc = sum(C_dict[ij] for ij in theta_i)
+        Li = sum(C_dict[ij] for ij in theta_i_star)
+        Wi = Wi_nc + Li 
+
+        # --------------------------------------------------------------------------
+        # IV. bound alpha and beta
+        # For Case A (has no delay to the critical path):
+        if (R_i_m_minus_one <= f_theta_i_star):
+            alpha_i = Wi_nc
+            beta_i = 0
+
+        # For Case B (has delay to the critical path):
+        else:
+            print("Case B")
+            # get len_lamda_ve
+            ve = f_v_i_max_idx
+            print(ve)
+
+            len_lamda_ve = 0
+            beta_i = min(R_i_m_minus_one - f_theta_i_star, len_lamda_ve)
 
 
-    # get m_i
-
-    # calc finish time
-
-    # Case A:
-    # calc alpha
-    # calc beta
 
 
-    # Case B:
-    
-    return R, alpha, beta
+
+            alpha_i_hat = 0
+            alpha_i_new = 0
+
+
+
+
+
+
+
+            alpha_i = max(alpha_i_hat, alpha_i_new)
+        
+        # calculate the response time based on alpha_i and beta_i
+        Ri = Li + 1.0 / m * (Wi - Li - max(alpha_i - (m - 1) * beta_i, 0))
+        print("Ri:", Ri)
+        f_offest = f_offest + Ri
+
+        alpha_sum = alpha_sum + alpha_i
+        beta_sum = beta_sum + beta_i
+
+    R = f_offest
+
+    return R, alpha_sum, beta_sum
 
 
 if __name__ == "__main__":
-    R, alpha, beta = rta_new_v2(task_idx=3, m=4)
-    print("R={}, alpha={}, beta={}".format(R, alpha, beta))
+    task_idx = 10; m = 4 # (4) (6 10)
+
+    R0 = rta_classic(task_idx, m)
+    R, alpha, beta = rta_new_v2(task_idx, m)
+    print("R0 = {}".format(R0))
+    print("R1 = {}, alpha = {}, beta = {}".format(R, alpha, beta))
